f582400443d17177fd0fb817419a45cf1f4dc764
gwasa211 <gdyjc2521@gmail.com>
2025-04-14 16:00:26 +0900

    [Feat}] if문 for문 배열 활용해서 Tile 생성

---
+++ Assets/CubeGameUi.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CubeGameUi : MonoBehaviour
+{
+    public TextMeshProUGUI TimerText;
+    public float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        TimerText.text = "생존 시간 :" + Timer.ToString("0.00");
+    }
+}
