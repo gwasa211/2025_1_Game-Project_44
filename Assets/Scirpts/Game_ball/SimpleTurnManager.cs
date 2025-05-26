4f495c42abdae5e69e4ca2b1e4d0f6398ccdf4fa
gwasa211 <gdyjc2521@gmail.com>
2025-05-26 15:52:27 +0900

    [Feat] 당구 게임 턴 만들기

---
+++ Assets/Scirpts/Game_ball/SimpleTurnManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTurnManager : MonoBehaviour
+{
+    public static bool canPlay = true;
+    public static bool anyBallMoving = false;
+
+    
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAllBall();
+        if(!anyBallMoving && !canPlay)
+        {
+            canPlay = true;
+            Debug.Log("턴 종료! 다시 칠 수 있습니다. ");
+
+        }
+    }
+
+    void CheckAllBall()
+    {
+        SimpleBallController[] allBalls = FindObjectsOfType<SimpleBallController>();
+        anyBallMoving = false;
+
+        foreach (SimpleBallController ball in allBalls)
+        {
+            if(ball.IsMoving())
+            {
+                anyBallMoving = true;
+                break;
+            }
+        }
+    }
+
+
+    public static void OnBallHit()
+    {
+        canPlay = false;
+        anyBallMoving = true;
+        Debug.Log("턴 시작! 공이 멈출 때 까지 기다리세요");
+    }
+}
